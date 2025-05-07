ec7363105d439ad3c5127ce0634b608f4d58451f
Dhruv Rathi <144977333+iamdhruvrathi@users.noreply.github.com>
2025-05-07 11:11:12 +0530

    Designed List Component

--- components/list/List.jsx
+++ components/list/List.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./list.css";
+import Userinfo from "./userinfo/USerinfo";
+import ChatList from "./chatList/Chatlist";
 
 const List = () => {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <Userinfo />
+      <ChatList />
+    </div>
+  );
 };
 
 export default List;
